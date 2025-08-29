6cb16a172baf7a7f73d4d82f4854321795460fb1
shrabonmolla <shrabonmolla007@gmail.com>
2025-08-29 19:59:13 +0600

    calling functionaly added

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-let hearcount = 0
-sum= 0
-hearticons = document.getElementsByClassName("fa-heart")
-heartDisplay = document.getElementById("heartdisplay")
+let hearcount = 0;
+hearticons = document.getElementsByClassName("fa-heart");
+heartDisplay = document.getElementById("heartdisplay");
 
-
-for(let icon of hearticons){
-    icon.addEventListener("click",function(){
-        hearcount++
-        heartDisplay.innerText= hearcount
-    })
+for (let icon of hearticons) {
+  icon.addEventListener("click", function () {
+    hearcount++;
+    heartDisplay.innerText = hearcount;
+  });
 }
+
+let coin = 100;
+
+const displaycoin = document.getElementById("displaycoin");
+document
+  .getElementById("card-container")
+  .addEventListener("click", function (e) {
+    if (e.target.tagName == "BUTTON") {
+      button = e.target;
+      serviceName =
+        button.parentNode.parentNode.children[1].children[0].innerText;
+      serviceNumber =
+        button.parentNode.parentNode.children[2].children[0].innerText;
+
+      if (coin < 20) {
+        alert("not enough coin available to make a call");
+        return;
+      }
+
+      alert(`${serviceName} ${serviceNumber} is calling`);
+      coin -= 20;
+      displaycoin.innerText = coin;
+    }
+  });
